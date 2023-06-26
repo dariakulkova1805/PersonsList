f8d8eb00dd49be1e794be75eff00e744f38cdf9e
Daria Kulkova <d.kulkovva@icloud.com>
2023-06-26 14:28:41 +0300

    Initial Commit

---
+++ PersonsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonsList
+//
+//  Created by DARYA on 6/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
